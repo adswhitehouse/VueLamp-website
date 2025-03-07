d2bd91ee00ce80addca41c34a0e3011ceea9d82e
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-07 19:55:37 +0000

    made responsive and animated lamp elements

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Variables
-let lightToggle = document.querySelector(".jsLightToggle")
-let lampLight = document.querySelector(".jsLampLight")
+let lightToggle = document.querySelector(".jsLightToggle");
+let lampLight = document.querySelector(".jsLampLight");
+let menu = document.querySelector(".jsMenu");
+let ul = document.querySelector(".jsUl");
 
-// Toggles slide and background color transitions on light toggle button 
+// Toggles slide and background color transitions on light toggle button
 lightToggle.addEventListener("click", () => {
-  lightToggle.classList.toggle("light-switch")
-  lightToggle.classList.toggle("light-off")
-  lampLight.classList.toggle("light-opacity")
-})
+  lightToggle.classList.toggle("light-switch");
+  lightToggle.classList.toggle("light-off");
+  lampLight.classList.toggle("light-opacity");
+});
+
+// Shows and hides menu on smaller screens
+menu.addEventListener("click", () => {
+  ul.classList.toggle("show-nav");
+});
